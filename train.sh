17f34f7f47a2f93ac11d60b2b118714e29ac19a8
Rayan Samy Ramoul <raysamram@gmail.com>
2021-09-06 19:06:12 +0200

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-
 TRAINDATA="${HOME}/DATASETS/MODIS_EURE4CA/train_d"
 VALDATA="${HOME}/DATASETS/MODIS_EURE4CA/study_d"
 PATHSAVE="${HOME}/MODELS/autoencoder.model"
@@ -8,6 +6,4 @@ LR=0.05
 EPOCHS=100
 PYTHON="python"
 
-# with training the batch norm
-# 72.0 mAP
 $PYTHON train.py --train_dataset $TRAINDATA --val_dataset $VALDATA --path_save $PATHSAVE --lr $LR --epochs $EPOCHS
